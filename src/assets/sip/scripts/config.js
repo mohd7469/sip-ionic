a9685f1cd5416c693e2e78763cf26e1366f7fcb7
awais786327 <awais3227443@gmail.com>
2020-07-24 16:46:11 +0500

    implemented sip phone module

---
+++ src/assets/sip/scripts/config.js
@@ -0,0 +1,24 @@
+// var user = {
+//     //  User Name
+//     "User" : "sipuser",
+//     //  Password
+//     "Pass" : "yourpassword",
+//     //  Auth Realm
+//     "Realm"   : "sip.sample.com",
+//     // Display Name
+//     "Display" : "Gob Bleuth",
+//     // WebSocket URL
+//     "WSServer"  : "wss://wss.sample.com:8443"
+// };
+var user = {
+    //  User Name
+    "User" : "crescas",
+    //  Password
+    "Pass" : "ab*Nb67f",
+    //  Auth Realm
+    "Realm"   : "sip.crescas.net",
+    // Display Name
+    "Display" : "Crescas",
+    // WebSocket URL
+    "WSServer"  : "wss://sip.crescas.net:4433"
+};
