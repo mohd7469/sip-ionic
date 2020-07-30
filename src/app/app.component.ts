1e35a2c2dac05a19cf4f533344e79f50db927448
awais786327 <awais3227443@gmail.com>
2020-07-30 17:04:32 +0500

    New ionic Sip call implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,97 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
-import { Platform } from '@ionic/angular';
+import { Platform, AlertController } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+
+import {AndroidPermissions} from '@ionic-native/android-permissions/ngx';
+import {OpenNativeSettings} from '@ionic-native/open-native-settings/ngx';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  microphonePermission = null;
+
   constructor(
     private platform: Platform,
+    private androidPermissions: AndroidPermissions,
+    private alertController: AlertController,
+    private openNativeSettings: OpenNativeSettings,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar
   ) {
     this.initializeApp();
+    this.microphonePermission = this.androidPermissions.PERMISSION.RECORD_AUDIO;
   }
 
   initializeApp() {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+
+      /* Permission flow start here */
+      this.checkPermission();
     });
   }
+
+  ngOnInit(): void {
+    console.log(this.androidPermissions);
+  }
+
+  checkPermission() {
+    const onsuccess = (result) => {
+      if (!result.hasPermission) {
+        console.log('Has still not authorized yet : Check Permission Error ');
+        return this.requestForPermissions();
+      }
+      console.log('Has permission ', result.hasPermission);
+    };
+    const onerror = (err) => {
+      console.log('Check Permission Error ');
+      this.requestForPermissions();
+    };
+
+    this.platform.ready().then(() => {
+      console.log('Checking Android Permissions.. ');
+      this.androidPermissions.checkPermission(this.microphonePermission).then(onsuccess, onerror);
+    });
+  }
+
+  requestForPermissions() {
+    console.log('Requesting Permission..');
+
+    const onsuccess = (result) => {
+      if (!result.hasPermission) {
+        console.log('Has still not authorized yet : Request Permission Error ');
+        // return this.openSettings();
+      }
+      console.log('Permissions Granted ', result.hasPermission);
+    };
+    const onerror = (err) => {
+      console.log('Request Permission Error ');
+      // return this.openSettings();
+    };
+
+    this.androidPermissions.requestPermission(this.microphonePermission).then(onsuccess, onerror);
+  }
+
+  /*openSettings() {
+    this.alertController.create({
+      header: 'Could not start app.',
+      subHeader: 'Please enable microphone permissions in the system app settings.',
+      buttons: [
+        'Cancel',
+        {
+          text: 'Open App Settings',
+          handler: () => this.openNativeSettings.open('application_details')
+        }
+      ]
+    })
+        .then(alert => alert.present());
+  }*/
+
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,7 +1,11 @@
-import {Component, OnInit} from '@angular/core';
-import {Platform, AlertController} from '@ionic/angular';
-import {AndroidPermissions} from '@ionic-native/android-permissions/ngx';
-import {OpenNativeSettings} from '@ionic-native/open-native-settings/ngx';
+import { Component, OnInit, AfterViewInit, ChangeDetectorRef } from '@angular/core';
+
+import { Web } from 'sip.js';
+import { getAudio, getButton, getButtons, getInput, getSpan, getDiv } from './demo-utils';
+// @ts-ignore
+const { SimpleUser, SimpleUserDelegate, SimpleUserOptions } = Web;
+
+declare var $: any;
 
 @Component({
     selector: 'app-home',
@@ -9,80 +13,241 @@ import {OpenNativeSettings} from '@ionic-native/open-native-settings/ngx';
     styleUrls: ['home.page.scss'],
 })
 
-export class HomePage implements OnInit {
+export class HomePage implements OnInit, AfterViewInit {
+    isCalling = false;
+    isConnected = false;
 
-    /*
-    permissions = [
-        this.androidPermissions.PERMISSION.RECORD_AUDIO,
-        this.androidPermissions.PERMISSION.REQUEST_MICROPHONE,
-    ];
-    */
-    microphonePermission = null;
+    constructor(private cdr: ChangeDetectorRef) {}
 
-    constructor(
-        public platform: Platform,
-        private androidPermissions: AndroidPermissions,
-        private alertController: AlertController,
-        private openNativeSettings: OpenNativeSettings
-    ) {
-        this.microphonePermission = this.androidPermissions.PERMISSION.RECORD_AUDIO;
+    ngOnInit() {
     }
 
-    ngOnInit(): void {
-        this.checkPermission();
-        console.log(this.androidPermissions);
-    }
+    ngAfterViewInit() {
+        const statusDiv = getDiv('status');
+        // const serverSpan = getSpan('server');
+        // const targetSpan = getSpan('target');
+        // const connectButton = getButton('connect');
+        const callButton = getButton('call');
+        const hangupButton = getButton('hangup');
+        // const disconnectButton = getButton('disconnect');
+        const audioElement = getAudio('remoteAudio');
+        // const keypad = getButtons('keypad');
+        // const dtmfSpan = getSpan('dtmf');
+        const holdCheckbox = getInput('hold');
+        const muteCheckbox = getInput('mute');
 
-    checkPermission() {
-        const onsuccess = (result) => {
-            if (!result.hasPermission) {
-                console.log('Has still not authorized yet : Check Permission Error ');
-                return this.requestForPermissions();
+        const user = {
+            'User'      : 'crescas',                    // User Name
+            'Pass'      : 'ab*Nb67f',                   // Password
+            'Realm'     : 'sip.crescas.net',            // Auth Realm
+            'Display'   : 'Crescas',                    // Display Name
+            'WSServer'  : 'wss://sip.crescas.net:4433'  // WebSocket URL
+        };
+
+        const webSocketServer = user.WSServer;
+        // serverSpan.innerHTML = webSocketServer;
+
+        const target = 'sip:' + user.User + '@' + user.Realm;
+        // targetSpan.innerHTML = target;
+
+        statusDiv.innerHTML = 'Connecting..';
+        this.isConnected = false;
+        this.cdr.detectChanges();
+
+        // audioElement.hidden = true;
+
+        const displayName = user.Display;
+
+        // @ts-ignore
+        const simpleUserDelegate: SimpleUserDelegate = {
+            onCallCreated: (): void => {
+                console.log(`[${displayName}] Call created`);
+                callButton.disabled = true;
+                this.isCalling = true;
+                this.cdr.detectChanges();
+                hangupButton.disabled = false;
+                // keypadDisabled(true);
+                holdCheckboxDisabled(true);
+                muteCheckboxDisabled(true);
+            },
+            onCallAnswered: (): void => {
+                console.log(`[${displayName}] Call answered`);
+                // keypadDisabled(false);
+                holdCheckboxDisabled(false);
+                muteCheckboxDisabled(false);
+            },
+            onCallHangup: (): void => {
+                console.log(`[${displayName}] Call hangup`);
+                callButton.disabled = false;
+                this.isCalling = false;
+                this.cdr.detectChanges();
+                hangupButton.disabled = true;
+                // keypadDisabled(true);
+                holdCheckboxDisabled(true);
+                muteCheckboxDisabled(true);
+            },
+            onCallHold: (held: boolean): void => {
+                console.log(`[${displayName}] Call hold ${held}`);
+                holdCheckbox.checked = held;
             }
-            console.log('Has permission ', result.hasPermission);
-        };
-        const onerror = (err) => {
-            console.log('Check Permission Error ');
-            this.requestForPermissions();
         };
 
-        this.platform.ready().then(() => {
-            console.log('Checking Android Permissions.. ');
-            this.androidPermissions.checkPermission(this.microphonePermission).then(onsuccess, onerror);
-        });
-    }
-
-    requestForPermissions() {
-        console.log('Requesting Permission..');
-
-        const onsuccess = (result) => {
-            if (!result.hasPermission) {
-                console.log('Has still not authorized yet : Request Permission Error ');
-                // return this.openSettings();;
-            }
-            console.log('Permissions Granted ', result.hasPermission);
-        };
-        const onerror = (err) => {
-            console.log('Request Permission Error ');
-            // this.openSettings();
-        };
-
-        this.androidPermissions.requestPermission(this.microphonePermission).then(onsuccess, onerror);
-    }
-
-    openSettings() {
-        this.alertController.create({
-            header: 'Could not start app.',
-            subHeader: 'Please enable microphone permissions in the system app settings.',
-            buttons: [
-                'Cancel',
-                {
-                    text: 'Open App Settings',
-                    handler: () => this.openNativeSettings.open('application_details')
+        // @ts-ignore
+        const simpleUserOptions: SimpleUserOptions = {
+            delegate: simpleUserDelegate,
+            media: {
+                remote: {
+                    audio: audioElement
                 }
-            ]
-        })
-            .then(alert => alert.present());
+            },
+            userAgentOptions: {
+                displayName,
+                authorizationUsername: user.User,
+                authorizationPassword: user.Pass
+            }
+        };
+
+        const simpleUser = new SimpleUser(webSocketServer, simpleUserOptions);
+
+        // connectButton.addEventListener('click', () => {
+        // connectButton.disabled = true;
+        // disconnectButton.disabled = true;
+        callButton.disabled = true;
+        this.isCalling = true;
+        this.cdr.detectChanges();
+        hangupButton.disabled = true;
+        simpleUser
+            .connect()
+            .then(() => {
+                // connectButton.disabled = true;
+                // disconnectButton.disabled = false;
+                callButton.disabled = false;
+                this.isCalling = false;
+                hangupButton.disabled = true;
+                statusDiv.innerHTML = 'Connected';
+                this.isConnected = true;
+                this.cdr.detectChanges();
+            })
+            .catch((error: Error) => {
+                // connectButton.disabled = false;
+                console.error(`[${simpleUser.id}] failed to connect`);
+                console.error(error);
+                alert('Failed to connect.\n' + error);
+            });
+        // });
+
+        callButton.addEventListener('click', () => {
+            callButton.disabled = true;
+            this.isCalling = true;
+            this.cdr.detectChanges();
+            hangupButton.disabled = true;
+            simpleUser.call(target).catch((error: Error) => {
+                console.error(`[${simpleUser.id}] failed to place call`);
+                console.error(error);
+                alert('Failed to place call.\n' + error);
+            });
+        });
+
+        hangupButton.addEventListener('click', () => {
+            callButton.disabled = true;
+            this.isCalling = true;
+            this.cdr.detectChanges();
+            hangupButton.disabled = true;
+            simpleUser.hangup().catch((error: Error) => {
+                console.error(`[${simpleUser.id}] failed to hangup call`);
+                console.error(error);
+                alert('Failed to hangup call.\n' + error);
+            });
+        });
+
+        // disconnectButton.addEventListener('click', () => {
+        //   // connectButton.disabled = true;
+        //   // disconnectButton.disabled = true;
+        //   callButton.disabled = true;
+        //   hangupButton.disabled = true;
+        //   simpleUser
+        //     .disconnect()
+        //     .then(() => {
+        //       // connectButton.disabled = false;
+        //       // disconnectButton.disabled = true;
+        //       callButton.disabled = true;
+        //       hangupButton.disabled = true;
+        //     })
+        //     .catch((error: Error) => {
+        //       console.error(`[${simpleUser.id}] failed to disconnect`);
+        //       console.error(error);
+        //       alert('Failed to disconnect.\n' + error);
+        //     });
+        // });
+
+        // keypad.forEach((button) => {
+        //   button.addEventListener('click', () => {
+        //     const tone = button.textContent;
+        //     if (tone) {
+        //       simpleUser.sendDTMF(tone).then(() => {
+        //         dtmfSpan.innerHTML += tone;
+        //       });
+        //     }
+        //   });
+        // });
+
+        // const keypadDisabled = (disabled: boolean): void => {
+        //   keypad.forEach((button) => (button.disabled = disabled));
+        //   dtmfSpan.innerHTML = '';
+        // };
+
+        holdCheckbox.addEventListener('change', () => {
+            if (holdCheckbox.checked) {
+                // Checkbox is checked..
+                simpleUser.hold().catch((error: Error) => {
+                    holdCheckbox.checked = false;
+                    console.error(`[${simpleUser.id}] failed to hold call`);
+                    console.error(error);
+                    alert('Failed to hold call.\n' + error);
+                });
+            } else {
+                // Checkbox is not checked..
+                simpleUser.unhold().catch((error: Error) => {
+                    holdCheckbox.checked = true;
+                    console.error(`[${simpleUser.id}] failed to unhold call`);
+                    console.error(error);
+                    alert('Failed to unhold call.\n' + error);
+                });
+            }
+        });
+
+        const holdCheckboxDisabled = (disabled: boolean): void => {
+            holdCheckbox.checked = false;
+            holdCheckbox.disabled = disabled;
+        };
+
+        muteCheckbox.addEventListener('change', () => {
+            if (muteCheckbox.checked) {
+                // Checkbox is checked..
+                simpleUser.mute();
+                if (simpleUser.isMuted() === false) {
+                    muteCheckbox.checked = false;
+                    console.error(`[${simpleUser.id}] failed to mute call`);
+                    alert('Failed to mute call.\n');
+                }
+            } else {
+                // Checkbox is not checked..
+                simpleUser.unmute();
+                if (simpleUser.isMuted() === true) {
+                    muteCheckbox.checked = true;
+                    console.error(`[${simpleUser.id}] failed to unmute call`);
+                    alert('Failed to unmute call.\n');
+                }
+            }
+        });
+
+        const muteCheckboxDisabled = (disabled: boolean): void => {
+            muteCheckbox.checked = false;
+            muteCheckbox.disabled = disabled;
+        };
+
+        // connectButton.disabled = false;
+
     }
 
 }
